59c93f48df93e804d654d273ba23c95d01ef8baa
gustavodsantos <gustavojuniordos@hotmail.com>
2024-11-15 20:55:00 -0300

    adcionado views, templates, formularios e tabelas.

--- mysite/base/urls.py
+++ mysite/base/urls.py
@@ -5,5 +5,15 @@ from mysite.base import views
 app_name = 'base'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home'),  # Página inicial
+    path('desafios/', views.listar_desafios, name='listar_desafios'),
+    path('desafios/<int:id>/', views.detalhes_desafio, name='detalhes_desafio'),
+    path('desafios/<int:id>/aceitar/', views.aceitar_desafio, name='aceitar_desafio'),
+    path('desafios/atribuir/', views.atribuir_desafio, name='atribuir_desafio'),
+    path('usuarios/', views.gerenciar_usuarios, name='gerenciar_usuarios'),
+    path('usuarios/cadastar/', views.cadastrar_corretor, name='cadastrar_corretor'),
+    path('usuarios/<int:id>/editar/', views.editar_usuario, name='editar_usuario'),
+    path('desafios/cadastrar/', views.cadastrar_desafio, name='cadastrar_desafio'),
+    path('desafios/atribuidos/', views.visualizar_desafios_atribuidos, name='visualizar_desafios_atribuidos'),
+    path('logged_out/', views.logged_out, name='logged_out'),
 ]
